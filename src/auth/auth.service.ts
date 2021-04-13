c90eb645cd19a8e401956967fc203c9822b00394
Smeekl <fx.smeekl@gmail.com>
2021-04-14 02:35:33 +0300

    feat: add signup route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,47 +13,52 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  create(user: CreateUserDto) {
-    return 'This action adds a new auth';
-    /*if (!userExist) {
+  async create(user: CreateUserDto) {
+    const { email, password } = user;
+    const userExist = await this.userService.findByEmail(email);
+
+    if (!userExist) {
+      const payload = { username: email };
+      const token = this.jwtService.sign(payload);
+
       let usernameRegex = /^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/;
       if (
-          user.email.match(usernameRegex) !== null &&
-          user.password.match(usernameRegex) !== null
+        email.match(usernameRegex) !== null &&
+        password.match(usernameRegex) !== null
       ) {
         await this.userService.create(user);
         const validUser = await this.validateUser({
-          email: user.email,
-          password: createHash('sha256').update(user.password).digest('hex'),
+          email,
+          password: createHash('sha256').update(password).digest('hex'),
         });
-        await this.userService.update(validUser.id, { token: accessToken });
-        return accessToken;
-      } else if (user.email.length < 3) {
+
+        await this.userService.update(validUser.id, { token: token });
+        return token;
+      } else if (email.length < 3) {
         throw new UnauthorizedException(
-            'email must contain more than 3 characters',
+          'email must contain more than 3 characters',
         );
-      } else if (user.password.length <= 0) {
+      } else if (password.length <= 0) {
         throw new UnauthorizedException(
-            'Password must contain more than 1 characters',
+          'Password must contain more than 1 characters',
         );
-      } else if (user.email.match(usernameRegex) === null) {
+      } else if (email.match(usernameRegex) === null) {
         throw new UnauthorizedException(
-            'Your username is not valid. Only characters A-Z, a-z, 0-9 and should doesnt contains special characters.',
+          'Your username is not valid. Only characters A-Z, a-z, 0-9 and should doesnt contains special characters.',
         );
-      } else if (user.password.match(usernameRegex) === null) {
+      } else if (password.match(usernameRegex) === null) {
         throw new UnauthorizedException(
-            'Your password is not valid. Only characters A-Z, a-z, 0-9 and should doesnt contains special characters.',
+          'Your password is not valid. Only characters A-Z, a-z, 0-9 and should doesnt contains special characters.',
         );
       }
-    }*/
+    }
   }
 
   async login(user: LoginDto) {
     const { email, password } = user;
-    const payload = { username: user.email };
+    const payload = { username: email };
     const userExist = await this.userService.findByEmail(email);
 
-    console.log(this.jwtService);
     const token = this.jwtService.sign(payload);
 
     if (!userExist) {
